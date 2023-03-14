7bf6e6d16df03595b3570128edcc09c6b14033a1
Andrew Effendi <andrewamadeo20@gmail.com>
2023-03-14 13:43:52 -0400

    modularized all function

--- game.asm
+++ game.asm
@@ -9,57 +9,137 @@
 # 
 .eqv  	BASE_ADDRESS	0x10008000 
 .eqv	COLOR_SPIKE	0xaaa9ad
-.eqv	COLOR_MONSTER	0xA64320
-.eqv	COLOR_RED	0x00112135
-.eqv	COLOR_BLUE	0x00112135
-.eqv	COLOR_GROUND	0x00112135
+.eqv	COLOR_MONSTER	0xa64320
+.eqv	COLOR_RED	0xff0000
+.eqv	COLOR_BLUE	0x0000ff
+.eqv	COLOR_GROUND	0x4cbb17
+.eqv	COLOR_PLATFORM	0x4cbb17
+.eqv	COLOR_FINISH	0xf5da0f
 
-.eqv	MONSTER_1_1	364
-.eqv	MONSTER_1_2	400
-.eqv	MONSTER_1_3	670
+.eqv	MONSTER_2_1	364
+.eqv	MONSTER_2_2	400
+.eqv	MONSTER_3_1	500
+.eqv	MONSTER_3_2	500
+.eqv	MONSTER_3_3	500
 .eqv	SPIKE_1_1	272
 .eqv	SPIKE_1_2	300
 .eqv	SPIKE_1_3	700
+.eqv	SPIKE_2_1	272
+.eqv	SPIKE_2_2	300
+.eqv	SPIKE_3_1	300
+.eqv	SPIKE_3_2	300
+.eqv	PLATFORM_1_1	600
+.eqv	PLATFORM_1_2	600
+.eqv	PLATFORM_2_1	700
+.eqv	PLATFORM_2_2	700
+.eqv	PLATFORM_3_1	900
+.eqv	PLATFORM_3_2	900
+.eqv	PLATFORM_3_3	600
+.eqv	GROUND		3968
+.eqv	FINISH_1	4000
+.eqv	FINISH_2	4020
+.eqv	FINISH_3	4040
+
  
 .text 
 	li $t0, BASE_ADDRESS # $t0 stores the base address for display 
  	
 reset:	li $s0, 100 # health
+	li $s7, 1 # level
+
 next_level:
  	li $s1, 0 # player's x-coordinate
  	li $s2, 108 # player's y-coordinate
+
+ 	li $s4, 0 # enemy 1
+ 	li $s5, 0 # enemy 2
+ 	li $s6, 0 # spike 3
  	
- 	li $s3, 0 # platform 1
- 	li $s3, 0 # platform 2
- 	li $s4, 0 # platform 3
- 	
- 	li $s5, 0 # spike 1
- 	li $s6, 0 # spike 2
- 	li $s7, 1 # level
- 	
- 	 j paint_ground 
+ground:	la $a1, COLOR_GROUND
+	la $a0, GROUND
+ 	j paint_ground
+ 
+# spike function
+spike:	la $a1, COLOR_SPIKE
+	beq $s7, 2, spike_lvl_2
+	beq $s7, 3, spike_lvl_3
 spike_lvl_1:
-	la $a1, COLOR_SPIKE
 	la $a0, SPIKE_1_1
 	j paint_spike
 	la $a0, SPIKE_1_2
 	j paint_spike
 	la $a0, SPIKE_1_3
-	j paint_spike 
-platform_lvl_1:	 
+	j paint_spike
+spike_lvl_2:
+	la $a0, SPIKE_2_1
+	j paint_spike
+	la $a0, SPIKE_2_2
+	j paint_spike
+spike_lvl_3:
+	la $a0, SPIKE_3_1
+	j paint_spike
+	la $a0, SPIKE_3_2
+	j paint_spike
+	
+# platform function
+platform: 
+	la $a1, COLOR_PLATFORM
+	beq $s7, 2, platform_lvl_2
+	beq $s7, 3, platform_lvl_3
+platform_lvl_1:
+	la $a0, PLATFORM_1_1
 	j paint_platform
+	la $a0, PLATFORM_1_2
+	j paint_platform
+platform_lvl_2:
+	la $a0, PLATFORM_2_1
+	j paint_platform
+	la $a0, PLATFORM_2_2
+	j paint_platform
+platform_lvl_3:
+	la $a0, PLATFORM_3_1
+	j paint_platform
+	la $a0, PLATFORM_3_2
+	j paint_platform
+
+# monster function
+monster:la $a1, COLOR_MONSTER
+	beq $s7, 2, monster_lvl_2
+	beq $s7, 3, monster_lvl_3
 monster_lvl_1:
-	la $a1, COLOR_MONSTER
-	la $a0, MONSTER_1_1
+	j finish # no monster for level 1
+monster_lvl_2:
+	la $a0, MONSTER_2_1
 	j paint_monster
-	la $a0, MONSTER_1_2
+	la $a0, MONSTER_2_2
 	j paint_monster
-	la $a0, MONSTER_1_3
+monster_lvl_3:
+	la $a0, MONSTER_3_1
 	j paint_monster
+	la $a0, MONSTER_3_2
+	j paint_monster
+	la $a0, MONSTER_3_3
+	j paint_monster
+
+# finish line function
+finish:	la $a1, COLOR_FINISH
+	beq $s7, 2, finish_lvl_2
+	beq $s7, 3, finish_lvl_3
+finish_lvl_1:
+	la $a0, FINISH_1
+	j paint_finish
+finish_lvl_2:
+	la $a0, FINISH_2
+	j paint_finish
+finish_lvl_3:
+	la $a0, FINISH_3
+	j paint_finish
+	
+# player function	
 player:
 	j paint_player
  
- wait:
+wait:
  	li $t9, 0xffff0000  
 	lw $t8, 0($t9) 
 	beq $t8, 1, keypress_happened 
@@ -102,17 +182,20 @@ right: 	addi $s1, $s1, 4 # x+4
 	j paint_player
 	
 respond_to_1:
+	li $s7, 1
 	j remove_player
 respond_to_2:
+	li $s7, 2
 	j remove_player
 respond_to_3:
+	li $s7, 3
 	j remove_player
 
 	
 remove_player:
-	li $t7, 32 #t7 = 64
-	mul $t7, $t7, $s2 #t7 = 256/64*y = 64*y
-	add $t7, $s1, $t7 #t7 = x + 64*y
+	li $t7, 32 	  #t7 = 32
+	mul $t7, $t7, $s2 #t7 = 32*y
+	add $t7, $s1, $t7 #t7 = x + 32*y
 	add $t7, $t7, $t0
 	li $t6, 0x000000
 	sw $t6, ($t7)
@@ -124,10 +207,10 @@ remove_player:
 	sw $t6, 260($t7)
 	sw $t6, 384($t7)
 	sw $t6, 388($t7)
-	beq $t8, 0x77, jump   # ASCII code of 'w' is 0x77 
-	beq $t8, 0x61, left   # ASCII code of 'a' is 0x61
-	beq $t8, 0x73, gravity   # ASCII code of 's' is 0x73 
-	beq $t8, 0x64, right   # ASCII code of 'd' is 0x64
+	beq $t8, 0x77, jump   	# ASCII code of 'w' is 0x77 
+	beq $t8, 0x61, left   	# ASCII code of 'a' is 0x61
+	beq $t8, 0x73, gravity  # ASCII code of 's' is 0x73 
+	beq $t8, 0x64, right   	# ASCII code of 'd' is 0x64
 	beq $t8, 0x31, next_level   # ASCII code of '31' is 0x64
 	beq $t8, 0x32, next_level   # ASCII code of '32' is 0x64
 	beq $t8, 0x33, next_level   # ASCII code of '33' is 0x64
@@ -151,48 +234,58 @@ paint_player:
 	j wait
 	
 paint_ground:
-	li $t6, 0x00ff00
+	add $a0, $t0, $a0 # a0 = base + coordinate
     	li $t7, 0
 loop_ground:
-	bge $t7, 32, spike_lvl_1
-	li $t5, 4
-	mul $t5, $t5, $t7
-	add $t5, $t0 $t5
-	addi $t5, $t5, 3968
-	sw $t6, ($t5)
+	bge $t7, 32, spike
+	li $t5, 4 # 4 bytes
+	mul $t5, $t5, $t7 #offset = 4 bytes * index
+	
+	add $t5, $t5, $a0 # t5 = address + offset
+	sw $a1, ($t5)
     	addi $t7, $t7, 1
     	j loop_ground	
     	
 paint_spike:
 	# $a0: position
 	# $a1: colour
-	add $a0, $t0, $a0 # t5 = base + coordinate
+	add $a0, $t0, $a0 # a0 = base + coordinate
 	sw $a1, 4($a0)
 	sw $a1, 128($a0)
 	sw $a1, 132($a0)
-	j platform_lvl_1
+	j platform
 	
 paint_platform:
-	li $t6, 0xA64320
-    	li $t7, 0 # init t7 = spike
+	# $a0: position
+	# $a1: colour
+	add $a0, $t0, $a0 # a0 = base + coordinate
+    	li $t7, 0 # init t7 = 0
 loop_platform:
-	bge $t7, 5, monster_lvl_1
-	li $t5, 4
-	mul $t5, $t5, $t7
-	add $t5, $t0 $t5
-	addi $t5, $t5, 3968
-	sw $t6, ($t5)
+	bge $t7, 9, monster
+	li $t5, 4 # 4 bytes
+	mul $t5, $t5, $t7 #offset = 4 bytes * index
+	
+	add $t5, $t5, $a0 # t5 = address + offset
+	sw $a1, ($t5)
     	addi $t7, $t7, 1
     	j loop_platform
 	
 paint_monster:
 	# $a0: position
 	# $a1: colour
-	add $a0, $t0, $a0 # t5 = base + coordinate
+	add $a0, $t0, $a0 # a0 = base + coordinate
 	sw $a1, ($a0)
 	sw $a1, 4($a0)
 	sw $a1, 128($a0)
 	sw $a1, 132($a0)
+	j finish
+
+paint_finish:
+	# $a0: position
+	# $a1: colour
+	add $a0, $t0, $a0 # a0 = base + coordinate
+	sw $a1, ($a0)
+	sw $a1, 4($a0)
 	j player
 	
 Exit:
